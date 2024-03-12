19af8a6b455c62800f940297e85aae6dec1e0054
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2024-03-12 22:17:26 +0000

    Removed unrequired css from logo

--- src/Logo/logo.jsx
+++ src/Logo/logo.jsx
@@ -3,14 +3,14 @@ import "./logo.css"
 const Logo = () => {
 
     return (
-    <div className="logo">
-        <div className="logo__line-container">
-            <div className="logo__line"></div>
-            <div className="logo__line"></div>
-            <div className="logo__line"></div>
+        <div className="logo">
+            <div className="logo__line-container">
+                <div className="logo__line"></div>
+                <div className="logo__line"></div>
+                <div className="logo__line"></div>
+            </div>
+            <h1>Kanban</h1>
         </div>
-        <h1>Kanban</h1>
-    </div>
     )
 }
 
--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,9 +1,11 @@
 import "./header.css"
 
+import Logo from "../Logo/logo"
+
 const Header = () => {
     return (
         <header className="Header">
-            {/* h2 is place holder that will be set debending on current board */}
+            {/* h2 is place holder that will be set depending on current board */}
             <h2>Platform Launch</h2> 
             <button className="header__button">+ Add new task</button>
         </header>
